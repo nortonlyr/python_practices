8261d04134613049527e7ad050ed3e9ea61d6f6e
nortonlyr <nortonlyr@gmail.com>
2020-10-07 23:36:24 -0400

    updated

--- MachineLearning_EX/EX_001.py
+++ MachineLearning_EX/EX_001.py
@@ -9,3 +9,11 @@ data = pd.read_csv('twitter_sentiments.csv')
 
 # print(data.head())
 
+train, test = train_test_split(data, test_size = 0.2, stratify = data['label'], random_state=21)
+
+train.shape, test.shape
+
+tfidf_vectorizer = TfidfVectorizer(lowercase= True, max_features=1000, stop_words=ENGLISH_STOP_WORDS)
+
+# fit the object with the training data tweets
+tfidf_vectorizer.fit(train.tweet)
