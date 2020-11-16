582de7d5f07fcf7b1502ea2e70aeb20900ba84a7
nortonlyr <nortonlyr@gmail.com>
2020-11-16 00:27:31 -0500

    Update scrapy_01.py

--- Scrapy/scrapy_01.py
+++ Scrapy/scrapy_01.py
@@ -1,3 +1,12 @@
 import scrapy
 
 class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['heeps://blog.scrpainghub.com']
+
+    def parse(self, response):
+        for title in response.css('.post-header>h2'):
+            yield {'title': title.css('a :: text').get()}
+
+        for next_page in response.css('a.next-posts-link'):
+            yield response.follow(next_page, self.parse)
