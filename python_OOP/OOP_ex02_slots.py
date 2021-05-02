ff6a53f14fced717b0424ef59aaee80ae5a98fb8
nortonlyr <nortonlyr@gmail.com>
2021-05-01 21:03:31 -0400

    updated oop exercises

--- python_OOP/OOP_ex02_slots.py
+++ python_OOP/OOP_ex02_slots.py
@@ -1,9 +1,15 @@
 # reference: https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/09.%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E8%BF%9B%E9%98%B6.md
 
 class Person(object):
-    def __init__(self, name, age):
+
+    #限定Person對象衹能綁定_name, _age和_gender屬性
+    __slots__ = ('_name', '_age', '_gender')
+
+    def __init__(self, name, age, is_male, gender):
         self._name = name
         self._age = age
+        self._is_male = True
+        self._gender = gender
 
     # 訪問器 - getter方法
     @property
@@ -29,8 +35,8 @@ class Person(object):
 def main():
     person = Person('Peter', 22)
     person.play()
-    person.age = 10
-    person.play()
+    person._gender = 'male'
+    person._is_male = True
 
 if __name__ == '__main__':
     main()
