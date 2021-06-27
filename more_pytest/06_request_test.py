ca6240a880609667003c40666bb391287e3c593a
nortonlyr <Zcw99019!>
2021-06-26 21:35:16 -0400

    updated pytest introspective_fixture

---
+++ more_pytest/06_request_test.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_with_introspection(introspective_fixture):
+    print("\nRunning test_with_introspection...")
+    assert True
+
+
+@pytest.fixture
+def introspective_fixture(request):
+    """
+    The request fixture allows introspection into the
+    "requesting" test case
+    """
+    print("\n\nintrospective_fixture:")
+    print("...Called at {}-level scope".format(request.scope))
+    print("   ...In the {} module".format(request.scope))
+    print("     ...On the {} node".format(request.node))
